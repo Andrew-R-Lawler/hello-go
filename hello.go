657e9417c0f6d8df7bec9031fcb39fae1519a5d4
Andrew-R-Lawler <Andrew.R.Lawler@gmail.com>
2024-11-12 18:39:27 -0600

    updated program and test to accept multiple os arguments

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package hello
 
 import (
-	"fmt"
+	"strings"
 )
 
-func Say(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
-}
+func Say(names []string) string {
+	if len(names) == 0 {
+		names = []string{"world"}
+	}
+	
+	return "Hello, " + strings.Join(names, ", ") + "!" 
+ }
